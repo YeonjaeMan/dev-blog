6cfb30427c0b438700897649b6dd998dd46ff236
정연재 <76952340+YeonjaeMan@users.noreply.github.com>
2024-12-08 16:39:02 +0900

    second commit

---
+++ src/main/java/com/yeondoit/dev_blog/DevBlogApplication.java
@@ -0,0 +1,13 @@
+package com.yeondoit.dev_blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevBlogApplication.class, args);
+	}
+
+}
